8e87f7671aafaf74e0db2e186893e5957c684136
Jaineel Upadhyay <ujaineel@gmail.com>
2022-12-03 17:00:36 -0500

    DSB-8 Comment in app module about config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import appConfig from './config/env/app.config';
 
 @Module({
   imports: [
+    // This import allows us to fetch env/non-env variables without calling process.env everywhere.
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `src/config/env/.env.${process.env.NODE_ENV}`,
