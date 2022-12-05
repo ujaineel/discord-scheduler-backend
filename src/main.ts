fc018ce18961e3053b399affdacf9027ea134a99
Jaineel Upadhyay <32003330+ujaineel@users.noreply.github.com>
2022-12-05 00:57:14 -0500

    DSB-16 Add Pino Logger For Logging (#9)

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import { Logger as AppLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new AppLogger('App');
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  app.flushLogs();
   const configService = app.get(ConfigService);
   // Get port to be used to run app on.
   const port = configService.get('app.PORT');
-  console.log(`Listening at port: ${port}`);
+  if (process.env.NODE_ENV === 'local') {
+    logger.log({ port: port }, 'App Started and listening');
+  }
   await app.listen(port);
 }
 bootstrap();
